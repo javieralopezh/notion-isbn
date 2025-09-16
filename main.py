27506a47314fd9258120b8b8e2b47fcc11077838
javieralopezh <110779209+javieralopezh@users.noreply.github.com>
2025-09-16 17:17:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from pushbullet import Pushbullet
 SERVICE_NAME = "Notion Books"
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 DATABASE_ID = DATABASE_ID = "1a05f36e-12a5-81a9-b9a1-000ce23838de"
-USE_PUSHBULLET = os.getenv("no")  # Default to 'no' if USE_PUSHBULLET is not set
-USE_AWS = os.getenv("no")  # Default to 'no' if USE_AWS is not set
+USE_PUSHBULLET = "no"  # Default to 'no' if USE_PUSHBULLET is not set
+USE_AWS = "no"  # Default to 'no' if USE_AWS is not set
 
 GOOGLE_API_KEY = os.getenv("GoogleAPIKey")
 
